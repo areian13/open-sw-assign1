90a7ef42a15afc1ce05ddece644872579a71fdc7
Areian <iantyrho13@naver.com>
2024-10-05 19:31:12 +0900

    feature-branch2: update 14line

--- calc.py
+++ calc.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     a, b = map(int, input().split())
     print(add(a, b))
     print(sub(a, b))
-    print(mul(a, b))
+    print(sub(mul(a, b), div(a, b)))
     print(div(a, b))
