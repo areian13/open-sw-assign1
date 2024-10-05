814ecba5d2a61b558b989e757c9fcb25073b6f85
Areian <iantyrho13@naver.com>
2024-10-05 18:25:17 +0900

    feat: add mul, div

--- calc.py
+++ calc.py
@@ -2,8 +2,14 @@ def add(a, b):
     return a + b
 def sub(a, b):
     return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
     print(add(a, b))
     print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
